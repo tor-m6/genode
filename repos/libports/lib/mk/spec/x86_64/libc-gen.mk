1e0c67eefda0f5ac792d288ba557dd043cfcfa97
Alexander Tormasov <tor@innopolis.ru>
2021-03-18 11:29:20 +0300

    revert glibc port of set/get/swap/makecontext and test

--- repos/libports/lib/mk/spec/x86_64/libc-gen.mk
+++ repos/libports/lib/mk/spec/x86_64/libc-gen.mk
@@ -3,8 +3,7 @@ include $(REP_DIR)/lib/mk/libc-gen.inc
 LIBC_GEN_AMD64_DIR = $(LIBC_DIR)/lib/libc/amd64/gen
 
 FILTER_OUT_S += rfork_thread.S setjmp.S _setjmp.S
-# do not use freebsd libc context function
-FILTER_OUT_C += _set_tp.c getcontextx.c signalcontext.c
+FILTER_OUT_C += _set_tp.c
 
 #
 # Filter functions conflicting with libm
@@ -14,9 +13,6 @@ FILTER_OUT_C += frexp.c
 
 SRC_S  = $(filter-out $(FILTER_OUT_S),$(notdir $(wildcard $(LIBC_GEN_AMD64_DIR)/*.S)))
 SRC_C += $(filter-out $(FILTER_OUT_C),$(notdir $(wildcard $(LIBC_GEN_AMD64_DIR)/*.c)))
-
-# add files for context switch
-SRC_S += __start_context.S  getcontext.S  setcontext.S  swapcontext.S
 
 vpath %.c $(LIBC_GEN_AMD64_DIR)
 vpath %.S $(LIBC_GEN_AMD64_DIR)
